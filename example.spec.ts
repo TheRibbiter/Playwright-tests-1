93877955cd2b9b4d80afafbf185a0320cb285b4e
Mark Leiva <mark.leiva@gmail.com>
2025-02-07 00:28:53 -0500

    Add initial Playwright test file

---
+++ example.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test';
+
+test('example test', async ({ page }) => {
+  await page.goto('https://example.com');
+  const title = await page.title();
+  expect(title).toBe('Example Domain');
+});
